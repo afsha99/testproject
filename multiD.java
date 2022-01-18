6d06937fad6fb658e12e1b122976a1f60f50d52d
afsha99 <97824733+afsha99@users.noreply.github.com>
2022-01-18 21:36:52 +0530

    Add files via upload

---
+++ multiD.java
@@ -0,0 +1,19 @@
+package multidimension;
+
+public class multiD {
+
+	public static void main(String[] args) {
+		int a []= {30,40,50,60,70};
+		for(int i=0;i<5;i++) {
+			System.out.println("Elements of array a: "+a[i]);
+		}
+		int[][] b= {
+				{2,4,6,8},
+				{3,6,9,5}};
+		System.out.println("Length of row 1: "+b[0].length);
+	
+		}
+
+	}
+
+
