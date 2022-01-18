298dce90161fa6417a2d7617d755095c867c2c78
afsha99 <97824733+afsha99@users.noreply.github.com>
2022-01-19 02:28:18 +0530

    Add files via upload

---
+++ DefaultConst.java
@@ -0,0 +1,14 @@
+package constructor;
+
+public class DefaultConst {
+	int a;
+	boolean b;
+
+	public static void main(String[] args) {
+		DefaultConst obj = new DefaultConst();
+		System.out.println("Default Value");
+        System.out.println("a ="+obj.a);
+        System.out.println("b=" +obj.b);
+	}
+
+}
