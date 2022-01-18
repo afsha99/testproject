ef5a9a084781a75b4798d13897299aeaf44000ec
afsha99 <97824733+afsha99@users.noreply.github.com>
2022-01-18 21:41:21 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,17 @@
+package arraystrngs;
+
+public class Array {
+
+	public static void main(String[] args) {
+		int a[]= {30, 40, 50,60,70};
+		for(int i=0;i<5;i++) {
+			System.out.println("Elements of array a: "+a[i]);
+		}
+   int[][] b = {
+		{5,6,7,8},
+		{1,2,3,4} };
+		System.out.println("Length of row 1: "+ b[0].length);
+
+}
+
+}
