cb7c518af9bb6d7eaa5af3312ce419a6d3c31945
afsha99 <97824733+afsha99@users.noreply.github.com>
2022-01-19 01:23:47 +0530

    Add files via upload

---
+++ primitive_datatype.java
@@ -0,0 +1,16 @@
+package primitive_dataytpe;
+
+public class primitive_datatype {
+
+	public static void main(String[] args) {
+		String s1="600";
+		String s2="700";
+		int a,b;
+		a=Integer.parseInt(s1);
+		b=Integer.parseInt(s2);
+		int sum = a + b;
+		System.out.println("Sum"+sum);
+
+	}
+
+}
