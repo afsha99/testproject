d1951595434d63d0be82fdd12a7c5ec0f7bc10c2
afsha99 <97824733+afsha99@users.noreply.github.com>
2022-01-28 23:55:06 +0530

    Add files via upload

---
+++ StackOperation.java
@@ -0,0 +1,52 @@
+package java_project;
+
+public class StackOperation {
+	static final int MAX = 1000; 
+	int top; 
+	int a[] = new int[MAX];  
+	boolean isEmpty() 
+	{ 
+    		return (top < 0); 
+	} 
+	StackOperation() 
+	{ 
+    		top = -1; 
+	} 
+	boolean push(int x) 
+	{ 
+    		if (top >= (MAX-1)) 
+    		{ 
+        			System.out.println("Stack Overflow"); 
+        			return false; 
+    		} 
+    		else
+    		{ 
+        			a[++top] = x; 
+        			System.out.println(x + " pushed into stack"); 
+        			return true; 
+    		} 
+	} 
+int pop() 
+	{ 
+    		if (top < 0) 
+    		{ 
+        			System.out.println("Stack Underflow"); 
+        			return 0; 
+    		} 
+    		else
+    		{ 
+        			int x = a[top--]; 
+        			return x; 
+    		} 
+	} 
+
+	public static void main(String args[])
+{
+    		StackOperation s = new StackOperation(); 
+    		s.push(11); 
+    		s.push(15); 
+    		s.push(5); 
+    		System.out.println(s.pop() + " Popped from stack"); 
+	}
+}
+
